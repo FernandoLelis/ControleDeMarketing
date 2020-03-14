bcd24a35aa1b0d5bb8f656785d23100dfe6f165d
FernandoLelis <FernandoLelis@github.com>
2020-03-14 11:43:23 -0300

    buttom logo

--- ControleDeMarketing/View/TelaAdicionarMarca.Designer.cs
+++ ControleDeMarketing/View/TelaAdicionarMarca.Designer.cs
@@ -32,18 +32,20 @@
             this.labelNomeAddMarca = new System.Windows.Forms.Label();
             this.textBoxNomeAddMarca = new System.Windows.Forms.TextBox();
             this.labelCategoriaAddMarca = new System.Windows.Forms.Label();
-            this.textBoxCategoriaAddMarca = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelRelvanciaAddMarca = new System.Windows.Forms.Label();
+            this.comboBoxRelavanciaAddMarca = new System.Windows.Forms.ComboBox();
+            this.comboBoxCategoriaAddMarca = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(170, 284);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.button1.Location = new System.Drawing.Point(39, 161);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Logo";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // labelNomeAddMarca
@@ -77,32 +79,23 @@
             this.labelCategoriaAddMarca.TabIndex = 1;
             this.labelCategoriaAddMarca.Text = "Categoria:";
             // 
-            // textBoxCategoriaAddMarca
+            // labelRelvanciaAddMarca
             // 
-            this.textBoxCategoriaAddMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxCategoriaAddMarca.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.textBoxCategoriaAddMarca.Location = new System.Drawing.Point(159, 78);
-            this.textBoxCategoriaAddMarca.Name = "textBoxCategoriaAddMarca";
-            this.textBoxCategoriaAddMarca.Size = new System.Drawing.Size(130, 23);
-            this.textBoxCategoriaAddMarca.TabIndex = 2;
+            this.labelRelvanciaAddMarca.AutoSize = true;
+            this.labelRelvanciaAddMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelRelvanciaAddMarca.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.labelRelvanciaAddMarca.Location = new System.Drawing.Point(36, 112);
+            this.labelRelvanciaAddMarca.Name = "labelRelvanciaAddMarca";
+            this.labelRelvanciaAddMarca.Size = new System.Drawing.Size(82, 17);
+            this.labelRelvanciaAddMarca.TabIndex = 1;
+            this.labelRelvanciaAddMarca.Text = "Relevância:";
             // 
-            // label1
+            // comboBoxRelavanciaAddMarca
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.label1.Location = new System.Drawing.Point(36, 112);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(82, 17);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Relevância:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.comboBox1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.comboBoxRelavanciaAddMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxRelavanciaAddMarca.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.comboBoxRelavanciaAddMarca.FormattingEnabled = true;
+            this.comboBoxRelavanciaAddMarca.Items.AddRange(new object[] {
             "1",
             "2",
             "3",
@@ -113,10 +106,25 @@
             "8",
             "9",
             "10"});
-            this.comboBox1.Location = new System.Drawing.Point(159, 110);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(41, 24);
-            this.comboBox1.TabIndex = 3;
+            this.comboBoxRelavanciaAddMarca.Location = new System.Drawing.Point(159, 110);
+            this.comboBoxRelavanciaAddMarca.Name = "comboBoxRelavanciaAddMarca";
+            this.comboBoxRelavanciaAddMarca.Size = new System.Drawing.Size(41, 24);
+            this.comboBoxRelavanciaAddMarca.TabIndex = 3;
+            // 
+            // comboBoxCategoriaAddMarca
+            // 
+            this.comboBoxCategoriaAddMarca.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxCategoriaAddMarca.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.comboBoxCategoriaAddMarca.FormattingEnabled = true;
+            this.comboBoxCategoriaAddMarca.Items.AddRange(new object[] {
+            "Áudio",
+            "Iluminação",
+            "Painiéis LED",
+            "Diversos"});
+            this.comboBoxCategoriaAddMarca.Location = new System.Drawing.Point(159, 76);
+            this.comboBoxCategoriaAddMarca.Name = "comboBoxCategoriaAddMarca";
+            this.comboBoxCategoriaAddMarca.Size = new System.Drawing.Size(130, 24);
+            this.comboBoxCategoriaAddMarca.TabIndex = 3;
             // 
             // TelaAdicionarMarca
             // 
@@ -125,12 +133,12 @@
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(362, 368);
             this.Controls.Add(this.labelNomeAddMarca);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.textBoxCategoriaAddMarca);
+            this.Controls.Add(this.comboBoxCategoriaAddMarca);
+            this.Controls.Add(this.comboBoxRelavanciaAddMarca);
             this.Controls.Add(this.textBoxNomeAddMarca);
             this.Controls.Add(this.labelCategoriaAddMarca);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelRelvanciaAddMarca);
             this.MaximizeBox = false;
             this.MaximumSize = new System.Drawing.Size(378, 407);
             this.MinimizeBox = false;
@@ -150,8 +158,8 @@
         private System.Windows.Forms.Label labelNomeAddMarca;
         private System.Windows.Forms.TextBox textBoxNomeAddMarca;
         private System.Windows.Forms.Label labelCategoriaAddMarca;
-        private System.Windows.Forms.TextBox textBoxCategoriaAddMarca;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelRelvanciaAddMarca;
+        private System.Windows.Forms.ComboBox comboBoxRelavanciaAddMarca;
+        private System.Windows.Forms.ComboBox comboBoxCategoriaAddMarca;
     }
 }
