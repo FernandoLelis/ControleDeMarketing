251269c3486920a4da1f7d0028c5f1d468ab863b
Fernando Lélis <fernandolelisdarocha@gmail.com>
2020-03-16 12:21:25 -0300

    almoço

---
+++ ControleDeMarketing/Control/ManipulaCampanha.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using ControleDeMarketing.Model;
+using System.Windows.Forms;
+
+namespace ControleDeMarketing.Control
+{
+    class ManipulaCampanha
+    {
+        public void adicionarMarca()
+        {
+            SqlConnection cn = new SqlConnection(Conexao.conectar());
+            SqlCommand cmd = new SqlCommand("pAdicionarCampanha", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            try
+            {
+                cmd.Parameters.AddWithValue("@nomeMarca", Campanha.Marca);
+
+                cmd.Parameters.AddWithValue("@categoriaMarca", Marca.CategoriaMarca);
+                cmd.Parameters.AddWithValue("@imagemMarca", Marca.ImagemMarca);
+                cmd.Parameters.AddWithValue("@relevanciaMarca", Marca.RelevanciaMarca);
+
+                SqlParameter nv = cmd.Parameters.Add("@nomeMarca", SqlDbType.NVarChar);
+                nv.Direction = ParameterDirection.Output;
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("Cadastro realizado com sucesso");
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+    }
+}
